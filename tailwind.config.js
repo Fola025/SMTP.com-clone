4ac791db4daafad850863ee559880921173cadd0
fola025 <folaolu25@gmail.com>
2023-04-15 13:48:38 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "nav": "#0C2461",
+      }
+    },
+  },
+  plugins: [],
+}
